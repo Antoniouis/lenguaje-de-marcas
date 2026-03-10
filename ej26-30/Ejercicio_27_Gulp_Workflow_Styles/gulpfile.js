101142fbae633c6bea53bbe4fe76f03f4c58eca2
Antoniouis <antonioluismartosangulo@gmail.com>
2026-03-10 11:27:45 +0100

    RESUBIDA

--- ej26-30/Ejercicio_27_Gulp_Workflow_Styles/gulpfile.js
+++ ej26-30/Ejercicio_27_Gulp_Workflow_Styles/gulpfile.js
@@ -7,14 +7,13 @@ const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function compilarCSS() {
-  return src('src/scss/style.scss')
-    .pipe(sass())
+  return src('src/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(dest('dist/css'));
 }
 
-function dev() {
-  watch('src/**/*.scss', compilarCSS);
-}
+exports.css = compilarCSS;
 
-exports.compilarCSS = compilarCSS;
-exports.dev = dev;
+exports.default = function () {
+  watch('src/scss/*.scss', compilarCSS);
+}
