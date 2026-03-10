bf275c1ad2f9ccc02b8c91f70d6330baf7f0b119
Antoniouis <antonioluismartosangulo@gmail.com>
2026-03-10 12:30:08 +0100

    REPARADO

--- ej26-30/Ejercicio_30_Landing_Automatizada/gulpfile.js
+++ ej26-30/Ejercicio_30_Landing_Automatizada/gulpfile.js
@@ -1,45 +1,44 @@
-// Configurar Gulp para:
-// 1. SASS -> CSS
-// 2. JS -> dist/js
-// 3. HTML -> dist/
-// 4. Watch
-// gulpfile.js ultra simple para tu estructura
 const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 
-// Compilar SCSS
+// SCSS → CSS
 function styles() {
-  return src('src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  return src('src/scss/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(dest('dist/css'))
     .pipe(browserSync.stream());
 }
 
-// Copiar JS
+// JS → dist/js
 function scripts() {
   return src('src/js/**/*.js')
     .pipe(dest('dist/js'))
     .pipe(browserSync.stream());
 }
 
-// Copiar HTML
+// HTML → dist
 function html() {
   return src('src/*.html')
     .pipe(dest('dist'))
     .pipe(browserSync.stream());
 }
 
-// Watch + servidor
-function watchFiles() {
-  browserSync.init({ server: { baseDir: 'dist' } });
-  watch('src/scss/**/*.scss', styles);
-  watch('src/js/**/*.js', scripts);
+// Servidor + Watch
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    }
+  });
+
+  watch('src/scss/*.scss', styles);
+  watch('src/js/*.js', scripts);
   watch('src/*.html', html);
 }
 
 // Default
 exports.default = series(
   parallel(styles, scripts, html),
-  watchFiles
+  serve
 );
